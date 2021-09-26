4e9f7d64d4b4203ac8db3da5aafcdbb8392a74c3
João Pedro Amaral Dias <joaopedrodiasamaral@gmail.com>
2021-09-26 10:07:02 -0300

    add MulttiUsuariosService

---
+++ app/Services/MulttiUsuariosService.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\MulttiUsuarios;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class MulttiUsuariosService
+{
+    public function index()
+    {
+        try {
+            return MulttiUsuarios::select(array(
+                'id',
+                'nome',
+                'email',
+                'telefone',
+            ))
+            ->orderBy('nome', 'asc')
+            ->get();
+        } catch (\Throwable $th) {
+            throw ValidationException::withMessages([
+                "error" => $th,
+                "message" => $th->getMessage(),
+                "code" => $th->getCode()
+            ]);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            return MulttiUsuarios::create([
+                'nome' => $request->nome,
+                'email' => $request->email,
+                'senha' => Hash::make($request->senha),
+                'telefone' => $request->telefone,
+            ]);
+        } catch (\Throwable $th) {
+            throw ValidationException::withMessages([
+                "error" => $th,
+            ]);
+        }
+    }
+
+    public function show(int $id)
+    {
+        try {
+            return MulttiUsuarios::find($id);
+        } catch (\Throwable $th) {
+            throw ValidationException::withMessages([
+                "error" => $th,
+                "message"=>$th->getMessage(),
+                "code"=>$th->getCode(),
+            ]);
+        }
+    }
+
+    public function update($request, int $id)
+    {
+        try {
+            $usuario = MulttiUsuarios::find($id);
+
+            if($usuario) {
+                $usuario->nome = $request->nome;
+                $usuario->email = $request->email;
+                $usuario->telefone = $request->telefone;
+                $usuario->senha = Hash::make($request->senha);
+                $usuario = $usuario->save();
+            }
+            if($usuario){
+                return MulttiUsuarios::find($id);
+            } else {
+                return $usuario;
+            }
+
+        } catch (\Throwable $th) {
+            throw ValidationException::withMessages([
+                "error" => $th,
+                "message" => $th->getMessage(),
+                "code" => $th->getCode()
+            ]);
+        }        
+    }
+
+    public function delete(int $id)
+    {
+        try {
+            return MulttiUsuarios::where('id', $id)->delete();
+        } catch (\Throwable $th) {
+            throw ValidationException::withMessages([
+                "error" => $th,
+                "message" => $th->getMessage(),
+                "code" => $th->getCode()
+            ]);
+        }
+    }
+}
