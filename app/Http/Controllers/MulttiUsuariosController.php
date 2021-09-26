35ab850cb8e8be3df5c1fb92aa3add976e24fa59
João Pedro Amaral Dias <joaopedrodiasamaral@gmail.com>
2021-09-26 10:05:40 -0300

    add MulttiUsuariosController

---
+++ app/Http/Controllers/MulttiUsuariosController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UsuariosMulttiCollection;
+use App\Http\Requests\StoreMulttiUsuarioRequest;
+use App\Http\Requests\UpdateMulttiUsuariosRequest;
+use App\Http\Resources\UsuarioMulttiResource;
+use App\Services\MulttiUsuariosService;
+
+class MulttiUsuariosController extends Controller
+{
+    protected $multti_usuarios;
+
+    public function __construct(MulttiUsuariosService $multti_usuarios)
+    {
+        $this->multti_usuarios = $multti_usuarios;
+    }
+
+    public function index()
+    {
+        return new UsuariosMulttiCollection($this->multti_usuarios->index());
+    }
+
+    public function store(StoreMulttiUsuarioRequest $request)
+    {
+        return new UsuarioMulttiResource($this->multti_usuarios->store($request));
+    }
+
+    public function update(UpdateMulttiUsuariosRequest $request, $id)
+    {
+        return new UsuarioMulttiResource($this->multti_usuarios->update($request,$id));
+    }
+
+    public function show($id)
+    {
+        return new UsuarioMulttiResource($this->multti_usuarios->show($id));
+    }
+
+    public function destroy($id)
+    {
+        return response()->json($this->multti_usuarios->delete($id));
+    }
+
+}
